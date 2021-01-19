070d552b7c5de91698c4b640390c5c6fa849d8dd
Ivan Mincione <ivan.mincione92@outlook.it>
2021-01-19 16:12:46 +0100

    Add Controller and Model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Dress;
+
 class HomeController extends Controller
 {
-    //
+    public function index() {
+        $all_dress = Dress::all();
+
+        $data = [
+            "dresses" => $all_dress
+        ];
+        return view("home", $data);
+    }
+
+    public function contacts() {
+        return view("contacts");
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,15 @@
 
 @section("content")
     <h1>Home products</h1>
+
+    <ul>
+        @foreach ($dresses as $item)
+            <li>
+                <p>{{ $item->name }}</p>
+                <p> {{ $item->color}}</p>
+                <p>{{ $item->size}}</p>
+                <p>{{ $item->price}}</p>
+            </li>
+        @endforeach
+    </ul>
 @endsection
